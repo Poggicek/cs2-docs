f8690497c4448b09bfb8d195e1e829865ec9e10e
Poggu <poggu@seznam.cz>
2023-03-27 00:35:45 +0200

    rework game events

--- src/components/GameEvent/index.js
+++ src/components/GameEvent/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import styles from "./styles.module.css";
 
-export default function GameEvent({title, structure}) {
+export default function GameEvent({structure, comment}) {
 	return (
-		<table>
-			<tbody>
-				{Object.entries(structure).map(([key, value], index) => (
-					<tr className={styles.member} key={index}>
-						<td className={styles.type}>{value}</td>
-						<td className={styles.name}>{key}</td>
-					</tr>
-				))}
-			</tbody>
-		</table>
+		<>
+			{comment && <div className={styles.gameEventComment}><b>Note:</b> {comment.charAt(0).toUpperCase() + comment.slice(1)}</div>}
+			<table>
+				<tbody>
+					{Object.entries(structure).map(([key, value], index) => (
+						<tr className={styles.member} key={index}>
+							<td className={styles.type}>{value.value}</td>
+							<td className={styles.name}>{key}</td>
+							{value.comment ? <td className={styles.comment}>{value.comment}</td> : <td className={styles.comment}></td>}
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</>
 	);
 }
