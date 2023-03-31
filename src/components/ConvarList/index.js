1737fc66e5db92dad608a62757d4916c72bf67b3
Poggu <poggu@seznam.cz>
2023-04-01 00:25:30 +0200

    add convar/command search and filter

--- src/components/ConvarList/index.js
+++ src/components/ConvarList/index.js
@@ -1,13 +1,56 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import clsx from "clsx";
 import CONVARS from "./CONVARS.json"
 import { GetFlagOverrides } from "./../../utils/convar-flags"
+import styles from "./styles.module.css";
+import debounce from 'lodash.debounce';
+import { FLAGS } from "./../../utils/convar-flags"
 
 const sortedList = CONVARS.sort((a, b) =>
     a.name.localeCompare(b.name));
 
+function Filters({filter, setFilter})
+{
+	const changeHandler = (e) => {
+		setFilter((filter) => ({...filter, search: e.target.value}));
+	}
+
+	const onDebouncedChange = useCallback(
+    debounce(changeHandler, 300)
+  , []);
+
+	const onFilterChecked = (e) => {
+		setFilter((filter) => ({
+			...filter,
+			flags: e.target.checked ? [...filter.flags, e.target.id] : filter.flags.filter(f => f !== e.target.id)
+		}));
+	}
+
+	return (
+		<div className={styles.wrapper}>
+			<input onChange={onDebouncedChange} className={clsx("navbar__search-input", styles.input)} placeholder="Search" />
+
+			<div className={clsx("dropdown dropdown--hoverable", styles.extraFilters)}>
+				<button className="button button--secondary">Filters</button>
+				<ul className={clsx("dropdown__menu", styles.dropdown)}>
+					{Object.entries(FLAGS).map(([name, displayName], index) => (
+						<li className={styles.checkboxGroup}>
+							<label className={styles.label} for={name}>{displayName}</label>
+							<input onChange={onFilterChecked} className={styles.checkbox} id={name} type="checkbox" checked={filter.flags.includes(name)}/>
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	)
+}
+
 export default function ConvarList() {
+	const [filter, setFilter] = useState({search: "", flags: []});
+
 	return (
 		<>
+			<Filters filter={filter} setFilter={setFilter}/>
 			<table>
 				<thead>
 					<tr>
@@ -17,7 +60,7 @@ export default function ConvarList() {
 					</tr>
 				</thead>
 				<tbody>
-					{sortedList.map((r, index) => (
+				{sortedList.filter(r => r.name.includes(filter.search) && (r.flags.split(' ').filter(f => filter.flags.includes(f)).length > 0 || filter.flags.length === 0)).map((r, index) => (
 						<tr key={index}>
 							<td>{r.name} {r.default.toString()}</td>
 							<td>{r.description}{r.min && <strong><br/>Min: {r.min}</strong>} {r.max && <strong><br/>Max: {r.max}</strong>}</td>
--- src/utils/convar-flags.js
+++ src/utils/convar-flags.js
@@ -18,3 +18,33 @@ export const GetFlagOverrides = (flags) => {
 	}
 	return ret.join(' ');
 }
+
+export const FLAGS = {
+	"developmentonly": "Development only",
+	"gamedll": "Game DLL",
+	"clientdll": "Client DLL",
+	"hidden": "Hidden",
+	"protected": "Protected",
+	"sponly": "Singleplayer only",
+	"archive": "Archive",
+	"notify": "Notify",
+	"userinfo": "User info",
+	"something_that_hides": "Something that hides",
+	"unlogged": "Unlogged",
+	"missing1": "Missing 1",
+	"missing2": "Missing 2",
+	"missing3": "Missing 3",
+	"missing4": "Missing 4",
+	"replicated": "Replicated",
+	"cheat": "Cheat",
+	"per_user": "Per user",
+	"demo": "Demo",
+	"dontrecord": "Don't record",
+	"release": "Release",
+	"menubar_item": "Menubar item",
+	"not_connected": "Not connected",
+	"vconsole_fuzzy_matching": "Vconsole fuzzy matching",
+	"server_cannot_query": "Server cannot query",
+	"clientcmd_can_execute": "Clientcmd can execute",
+	"execute_per_tick": "Execute per tick",
+}
